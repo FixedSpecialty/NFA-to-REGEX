01e3a84552e722b713d5443e04709128382db7b2
Andre Murphy <andremurphy98@outlook.com>
2020-12-06 13:10:59 -0700

    final

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # CS361P3
+
+Project 3 - Regular Expressions
+Andre Murphy and Josh Dixon
+
+REGEX
+This program uses the recursive descent algorithm(DFS) to construct an NFA given a REGEX.
+
+HOW DOES IT WORK? 
+Given a package of Java files, such as drivers and interfaces to work off, we had to create one class, which was the RE.java file. The class parses through the regex and assigns associated values to each character in the REGEX. It hands the symbols such as '(,),*,|' generally with the 4 basic methods eat, peek, next, and more. The class essentially takes a character and turns it into a state with relating transitions from the leaves of the descent to the root; bottom up.
+
+USAGE 
+This program is run through the REDriver.
+
+This is used to link the jar file to your driver.
+javac -cp ".:./CS361FA.jar" re/REDriver.java
+
+Then, run the Driver with your test files.
+java -cp ".:./CS361FA.jar" re.REDriver ./tests/p3tc1.txt
+
+TESTING 
+For testing we used custom .txt files to verify if out output was correct.
--- re/RE.java
+++ re/RE.java
@@ -7,52 +7,92 @@ import fa.State;
 import fa.nfa.NFA;
 import fa.nfa.NFAState;
 
+/**
+ * Recursive Descent Parser for a Regex to an NFA
+ * @author Andre Murphy and Josh Dixon
+ *
+ */
 public class RE implements REInterface {
 
-	String input = "";
-	int count = 1;
+	String input = "";	
+	Integer stateNum = 0; //to iterate through regex and assign # value
 
+	/**
+	 * Constructor for regular expression
+	 * @param regEx
+	 */
 	public RE(String regEx) {
-		input = regEx;
+		input = regEx; //save input globally
 	}
 	
+	/**
+	 * returns the next item of input without consuming it
+	 * @return
+	 */
 	private char peek(){
 		return input.charAt(0);
 	}
+	
+	/**
+	 * consumes the next item of input, failing if not equal to item
+	 * @param c
+	 */
 	private void eat(char c){
 		if(peek() == c){
 			this.input = this.input.substring(1);
 		}	
+		else
+			throw new RuntimeException("Expected: " + c + "; got: " + peek());
 	}
+	
+	/**
+	 * returns the next item of input and consumes it
+	 * @return
+	 */
 	private char next(){
 		char c = peek();
 		eat(c);
 		return c;
 	}		
+	
+	/**
+	 * checks if there is more input available
+	 * @return
+	 */
 	private boolean more(){
 		return input.length()>0;
 	}
+	
+	/**
+	 * Constructs and NFA from given start states, final states and creates transitions
+	 * @return new NFA if more, old NFA if not
+	 */
 	private NFA regex(){
 		NFA term = term();
 		if(more() && peek() == '|'){
 			eat('|');
-			NFA regex = regex();
-			NFA addition = new NFA();
-			addition.addStartState("q"+ count);
-			String name = "q"+count;
-			count++;
-			addition.addNFAStates(term.getStates());
-			addition.addNFAStates(regex.getStates());
-			addition.addTransition(name, 'e', term.getStartState().getName());
-			addition.addTransition(name, 'e', regex.getStartState().getName());
-			addition.addAbc(term.getABC());
+			NFA regex = regex(); //recursive call
+			NFA addition = new NFA(); //create new NFA
+			String start = stateNum.toString(); 
+			addition.addStartState(start); //add start state
+			stateNum++;
+			addition.addNFAStates(term.getStates()); //add states from term
+			addition.addNFAStates(regex.getStates()); //add states from recursive call
+			addition.addTransition(start, 'e', regex.getStartState().getName()); //add transition from other NFAs
+			addition.addTransition(start, 'e', term.getStartState().getName()); //add transition from other NFAs
+			addition.addAbc(term.getABC()); //add OLD alphabet
 			addition.addAbc(regex.getABC());
 			return addition;
 		}
 		else{
 			return term;
 		}
-	}	
+	}
+	
+	/**
+	 * has to check that it has not reached the boundary of a term or the end of the input
+	 * @return NFA
+	 */
 	private NFA term(){
 		NFA factor = new NFA();
 		while (more() && peek() != ')' && peek() != '|'){
@@ -61,11 +101,17 @@ public class RE implements REInterface {
 				factor = nextFactor;
 			}
 			else{
-				factor = join(factor, nextFactor);
+				factor = join(factor, nextFactor); //concatenate both NFAs
 			}
 		}
 		return factor;
 	}
+	
+	/**
+	 * this is a regex in ( )
+	 * @default 
+	 * @return
+	 */
 	private NFA base(){
 		switch(peek()){
 			case '(':
@@ -73,22 +119,28 @@ public class RE implements REInterface {
 				NFA nfa = regex();
 				eat(')');
 				return nfa;
-			default:
+			default: //constructs an NFA from next char with two states
 				NFA nfa2 = new NFA();
-				String start ="q"+ count;
-				count++;
-				String finals ="q"+ count;
-				count++;
+				stateNum++;
+				String start = stateNum.toString(); //locate start state from counter
+				stateNum++;
+				String finals = stateNum.toString(); //locate final state from counter
 				nfa2.addStartState(start);
+				stateNum++;
 				nfa2.addFinalState(finals);
 				char in = next();
 				nfa2.addTransition(start, in, finals);
-				Set<Character> abc = new LinkedHashSet<Character>();
+				Set<Character> abc = new LinkedHashSet<Character>(); //create alphabet for NFA
 				abc.add(in);
 				nfa2.addAbc(abc);
 				return nfa2;
 		}
 	}
+	
+	/**
+	 * parse a base and then any number of Kleene stars
+	 * @return
+	 */
 	private NFA factor(){
 		NFA base = base();
 		while(more() && peek() == '*'){
@@ -97,28 +149,42 @@ public class RE implements REInterface {
 		}
 		return base;
 	}
+	
+	/**
+	 * concatenate two NFAs
+	 * @param first NFA: one
+	 * @param second NFA: two
+	 * @return combined NFA
+	 */
 	private NFA join(NFA one, NFA two){
-		one.addNFAStates(two.getStates());
-		for(State s: one.getFinalStates()){
+		Set<State> finals = one.getFinalStates();
+		one.addNFAStates(two.getStates());		
+		for(State s: finals){
 			((NFAState)s).setNonFinal();
 			one.addTransition(s.getName(), 'e', two.getStartState().getName());			
 		}
 		one.addAbc(two.getABC());
 		return one;
 	}
+	
+	/**
+	 * Handles the case of (A*)= e or A,AA,AAA,etc.
+	 * @param base
+	 * @return
+	 */
 	private NFA handleAsterisk(NFA base){
 		NFA nfa = new NFA();
-		String s = "q"+count;
-		count++;
-		String f = "q"+count;
-		count++;
-		nfa.addStartState(s);
-		nfa.addFinalState(f);
+		String s = stateNum.toString(); //locate start state with counter
+		stateNum++;
+		String f = stateNum.toString(); //locate final state with counter
+		stateNum++;
+		nfa.addStartState(s); //add to NFA
+		nfa.addFinalState(f); //add to NFA
 		nfa.addNFAStates(base.getStates());
-		nfa.addTransition(s,'e',f);
-		nfa.addTransition(f,'e',base.getStartState().getName());
-		nfa.addTransition(s,'e',base.getStartState().getName());
-		nfa.addAbc(base.getABC());
+		nfa.addTransition(s,'e',f); //add empty transition
+		nfa.addTransition(f,'e',base.getStartState().getName()); //add empty transition
+		nfa.addTransition(s,'e',base.getStartState().getName()); //link initial state to base NFA
+		nfa.addAbc(base.getABC()); //add old alphabet
 		for(State s1: base.getFinalStates()){
 			nfa.addTransition(s1.getName(), 'e', f);
 			for(State s2: nfa.getFinalStates()){
@@ -130,9 +196,10 @@ public class RE implements REInterface {
 		return nfa;
 	}
 
-	@Override
+	/**
+	 * Constructs NFA from regex using recursive descent
+	 */
 	public NFA getNFA() {
-		// TODO Auto-generated method stub
 		return regex();
 	}
 }
